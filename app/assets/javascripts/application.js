5ed28d1f0632eec23a39ec8f808dc6d9f1bc4619
Ian Kropp <iankropp@gmail.com>
2016-04-05 10:27:24 -0400

    Started angularfying the page.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,18 +23,25 @@
 
   adminApp.config(['$httpProvider',function($httpProvider){
 
-        $httpProvider.defaults.headers.get = {
-          "Accept":"application/json;charset=utf-8",
-          "Accept-Charset":"charset=utf-8"
-        };
+    $httpProvider.defaults.headers.get = {
+      "Accept":"application/json;charset=utf-8",
+      "Accept-Charset":"charset=utf-8"
+    };
 
-      }]
-  );
+  }]);
 
   adminApp.controller('artistBrowserController', function($scope,$http,$location){
 
     $scope.loadingImages = true;
-    $scope.fish = "trout";
+
+    $scope.artisticPeriods = [
+      {'name':'Neoclassical',
+        'imgSrc':'https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Jacques-Louis_David_-_Oath_of_the_Horatii_-_Google_Art_Project.jpg/1200px-Jacques-Louis_David_-_Oath_of_the_Horatii_-_Google_Art_Project.jpg',
+        'imgAlt':'Jacques-Louis David - Oath of the Horatii - Google Art Project.jpg'},
+      {'name':'Impressionistic',
+        'imgSrc':'https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Claude_Monet%2C_Impression%2C_soleil_levant.jpg/1200px-Claude_Monet%2C_Impression%2C_soleil_levant.jpg',
+        'imgAlt':'Claude Monet, Impression, soleil levant.jpg'}
+    ] 
 
   });
  
