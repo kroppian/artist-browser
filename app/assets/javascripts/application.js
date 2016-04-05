374612b255491972867e9ab70c390b1dfe472228
Ian Kropp <iankropp@gmail.com>
2016-04-05 12:37:36 -0400

    Added the hiding/unhiding mecanism.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -50,12 +50,37 @@
       {'name':'Neoclassical',
         'imgSrc':'https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Jacques-Louis_David_-_Oath_of_the_Horatii_-_Google_Art_Project.jpg/1200px-Jacques-Louis_David_-_Oath_of_the_Horatii_-_Google_Art_Project.jpg',
         'imgAlt':'Jacques-Louis David - Oath of the Horatii - Google Art Project.jpg',
-        'artists': $scope.neoclassicalArtistsStub},
+        'artists': $scope.neoclassicalArtistsStub,
+        'visible':false},
       {'name':'Impressionistic',
         'imgSrc':'https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Claude_Monet%2C_Impression%2C_soleil_levant.jpg/1200px-Claude_Monet%2C_Impression%2C_soleil_levant.jpg',
         'imgAlt':'Claude Monet, Impression, soleil levant.jpg',
-        'artists': $scope.neoclassicalArtistsStub }
+        'artists': $scope.neoclassicalArtistsStub,
+        'visible':false},
+      {'name':'Post Impressionistic',
+        'imgSrc':'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/1200px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg',
+        'imgAlt':'A painting of a scene at night with 11 swirly stars and a bright yellow crescent moon. In the background there are hills, in the middle ground there is a moonlit town with a church that has an elongated steeple, and in the foreground there is the dark green silhouette of a cypress tree.',
+        'artists': $scope.neoclassicalArtistsStub,
+        'visible':false}
       ] 
+
+
+    $scope.switchPeriod = function(period){
+
+      var wasVisible = period.visible;
+
+      for (i = 0; i < $scope.artisticPeriods.length; i++) {
+        
+        $scope.artisticPeriods[i].visible = false; 
+
+      }
+
+      if (! wasVisible){
+        period.visible = true;
+      }
+
+    };
+
   }   
 
   );
