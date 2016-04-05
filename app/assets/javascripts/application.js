2729d1c92476ae4ba84c3b1ea491d31978ed9fac
Ian Kropp <iankropp@gmail.com>
2016-04-05 11:13:11 -0400

    Pulled the periods and the artists back into angular. Tidied up the index's html

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -34,15 +34,30 @@
 
     $scope.loadingImages = true;
 
+    $scope.neoclassicalArtistsStub = [
+      {'name':'Jacques-Louis David', 'portraitSrc':'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c6/David_Self_Portrait.jpg/1200px-David_Self_Portrait.jpg', 'portraitAlt':'David Self Portrait.jpg', 'about':'An influential French painter in the Neoclassical style, considered to be the preeminent painter of the era. In the 1780s his cerebral brand of history painting marked...' },
+      {'name':'Antonio Canova', 'portraitSrc':'https://upload.wikimedia.org/wikipedia/commons/6/6b/Antonio_Canova_Selfportrait_1792.jpg', 'portraitAlt':'Antonio Canova Selfportrait 1792.jpg', 'about':'An Italian neoclassical sculptor, famous for his marble sculptures. Often regarded as the greatest of the neoclassical artists, his artwork was...' },
+      {'name':'Jean-Auguste-Dominique Ingres', 'portraitSrc':'https://upload.wikimedia.org/wikipedia/commons/9/94/Ingres%2C_Self-portrait.jpg', 'portraitAlt':'Ingres, Self-portrait.jpg', 'about':' A French Neoclassical painter. Although he considered himself to be a painter of history in the tradition of Nicolas Poussin...' },
+      {'name':'Jacques-Louis David', 'portraitSrc':'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c6/David_Self_Portrait.jpg/1200px-David_Self_Portrait.jpg', 'portraitAlt':'David Self Portrait.jpg', 'about':'An influential French painter in the Neoclassical style, considered to be the preeminent painter of the era. In the 1780s his cerebral brand of history painting marked...' },
+      {'name':'Antonio Canova', 'portraitSrc':'https://upload.wikimedia.org/wikipedia/commons/6/6b/Antonio_Canova_Selfportrait_1792.jpg', 'portraitAlt':'Antonio Canova Selfportrait 1792.jpg', 'about':'An Italian neoclassical sculptor, famous for his marble sculptures. Often regarded as the greatest of the neoclassical artists, his artwork was...' },
+      {'name':'Jean-Auguste-Dominique Ingres', 'portraitSrc':'https://upload.wikimedia.org/wikipedia/commons/9/94/Ingres%2C_Self-portrait.jpg', 'portraitAlt':'Ingres, Self-portrait.jpg', 'about':' A French Neoclassical painter. Although he considered himself to be a painter of history in the tradition of Nicolas Poussin...' },
+      {'name':'Jacques-Louis David', 'portraitSrc':'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c6/David_Self_Portrait.jpg/1200px-David_Self_Portrait.jpg', 'portraitAlt':'David Self Portrait.jpg', 'about':'An influential French painter in the Neoclassical style, considered to be the preeminent painter of the era. In the 1780s his cerebral brand of history painting marked...' },
+      {'name':'Antonio Canova', 'portraitSrc':'https://upload.wikimedia.org/wikipedia/commons/6/6b/Antonio_Canova_Selfportrait_1792.jpg', 'portraitAlt':'Antonio Canova Selfportrait 1792.jpg', 'about':'An Italian neoclassical sculptor, famous for his marble sculptures. Often regarded as the greatest of the neoclassical artists, his artwork was...' },
+      {'name':'Jean-Auguste-Dominique Ingres', 'portraitSrc':'https://upload.wikimedia.org/wikipedia/commons/9/94/Ingres%2C_Self-portrait.jpg', 'portraitAlt':'Ingres, Self-portrait.jpg', 'about':' A French Neoclassical painter. Although he considered himself to be a painter of history in the tradition of Nicolas Poussin...' }
+    ];
+
     $scope.artisticPeriods = [
       {'name':'Neoclassical',
         'imgSrc':'https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Jacques-Louis_David_-_Oath_of_the_Horatii_-_Google_Art_Project.jpg/1200px-Jacques-Louis_David_-_Oath_of_the_Horatii_-_Google_Art_Project.jpg',
-        'imgAlt':'Jacques-Louis David - Oath of the Horatii - Google Art Project.jpg'},
+        'imgAlt':'Jacques-Louis David - Oath of the Horatii - Google Art Project.jpg',
+        'artists': $scope.neoclassicalArtistsStub},
       {'name':'Impressionistic',
         'imgSrc':'https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Claude_Monet%2C_Impression%2C_soleil_levant.jpg/1200px-Claude_Monet%2C_Impression%2C_soleil_levant.jpg',
-        'imgAlt':'Claude Monet, Impression, soleil levant.jpg'}
-    ] 
+        'imgAlt':'Claude Monet, Impression, soleil levant.jpg',
+        'artists': $scope.neoclassicalArtistsStub }
+      ] 
+  }   
 
-  });
+  );
  
 })();
